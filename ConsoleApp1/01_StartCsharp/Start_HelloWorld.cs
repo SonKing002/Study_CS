8640b1ad1a38c728d88a7390884faa1ffa737f59
SonKing002 <101817311+SonKing002@users.noreply.github.com>
2024-02-23 17:54:45 +0900

    CS
    첫장 작업중

--- ConsoleApp1/01_StartCsharp/Start_HelloWorld.cs
+++ ConsoleApp1/01_StartCsharp/Start_HelloWorld.cs
@@ -1,12 +1,11 @@
 ﻿using System;
+//using 
 
+//클래스 
 public class Start_HelloWorld
 {
     /// <summary>
-    ///  Main함수 
-    ///  static으로 선언으로 한다. 
-    ///  모든 프로그램은 시작함수로 가진다.
-    ///  임의의 클래스 안에서 존재하며, 프로그램 내 한개만 존재한다 
+    ///  프로그램 시작하는 메서드
     /// </summary>
     /// <param name="arguements"></param>
     public static void Main(string[] args) 
